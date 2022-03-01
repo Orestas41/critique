9033a8b18fe8acc6b60298868dc4028ca9222210
Orestas Dulinskas <orestasdulinskas@gmail.com>
2022-03-01 12:15:18 +0000

    Added full CRUD functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,44 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useRef, useState } from "react";
+import ReactDOM, { render } from "react-dom";
 import "./index.css";
-
-class Title extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Critique</h1>
-        <h3>website description</h3>
-      </div>
-    );
-  }
-}
 
 class FilterableFilmTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       filterText: "",
+      name: "React",
+      showSearch: true,
+      showFilmTable: true,
+      showFilmInfo: false,
+      showReviewBox: false,
     };
 
+    this.showComponent = this.showComponent.bind(this);
     this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+
+    // handleShowFilmTable(film) {
+    //   this.setState({ categoryClicked: true });
+    //   this.setState({ category: filmCategory });
+
+    //   console.log(this.state.category);
+  }
+
+  showComponent(name) {
+    console.log(name);
+    switch (name) {
+      case "showSearch":
+        this.setState({ showSearch: !this.state.showSearch });
+        break;
+      case "showFilmTable":
+        this.setState({ showFilmTable: !this.state.showFilmTable });
+        break;
+      case "showFilmInfo":
+        this.setState({ showFilmInfo: !this.state.showFilmInfo });
+        break;
+      case "showReviewBox":
+        this.setState({ showReviewBox: !this.state.showReviewBox });
+    }
   }
 
   handleFilterTextChange(FT) {
@@ -30,28 +48,67 @@ class FilterableFilmTable extends React.Component {
   }
 
   render() {
+    const { showSearch, showFilmTable, showFilmInfo, showReviewBox } =
+      this.state;
+
     return (
       <div id="welcome">
         <Title />
-        <SearchBar
-          filterText={this.state.filterText}
-          onFilterTextChange={this.handleFilterTextChange}
-        />
+        {showSearch && (
+          <SearchBar
+            filterText={this.state.filterText}
+            onFilterTextChange={this.handleFilterTextChange}
+          />
+        )}
+        <AddReview />
+        <br />
+        <DeleteReview />
         <br></br>
-        <FilmTable
-          films={this.props.films}
-          filterText={this.state.filterText}
-        />
+        <br />
+        <UpdateReview />
+        {showFilmInfo && (
+          <FilmInfo actors={this.props.actors} reviews={this.props.reviews} />
+        )}
+        {showFilmTable && (
+          <FilmTable
+            filterText={this.state.filterText}
+            actors={this.props.actors}
+            reviews={this.props.reviews}
+            onClick={() => this.showComponent()}
+          />
+        )}
+        {showReviewBox && <Review />}
+      </div>
+    );
+  }
+}
+
+function changeStatus(props) {
+  return console.log("hi");
+}
+
+class Title extends React.Component {
+  render() {
+    return (
+      <div id="title">
+        <h1>Critique</h1>
+        <h3>website description</h3>
       </div>
     );
   }
 }
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+    };
+  }
   render() {
     const filterText = this.props.filterText;
     return (
-      <form>
+      <form id="search">
         <input
           type="text"
           placeholder="Search..."
@@ -63,73 +120,419 @@ class SearchBar extends React.Component {
   }
 }
 
+function AddReview() {
+  const baseURL = "http://localhost:8080/HomePage";
+  const film_film_id = useRef(null);
+  const post_review = useRef(null);
+  const [postResult, setPostResult] = useState(null);
+  const fortmatResponse = (res) => {
+    return JSON.stringify(res, null, 2);
+  };
+  async function postData() {
+    const id = film_film_id.current.value;
+    const rev = post_review.current.value;
+    const postData = {
+      filmId: film_film_id.current.value,
+      review: post_review.current.value,
+    };
+    try {
+      const res = await fetch(
+        `${baseURL}/AddReviews?film_film_id=${id}&customer_review=${rev}`,
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(postData),
+        }
+      );
+      if (!res.ok) {
+        const message = `An error has occured: %{res.status} - ${res.statusText}`;
+        throw new Error(message);
+      }
+      const data = await res.json();
+      const result = {
+        status: res.status + "-" + res.statusText,
+        headers: {
+          "Content-Type": res.headers.get("Constent-Type"),
+          "Content-Length": res.headers.get("Content-Length"),
+        },
+        data: data,
+      };
+      setPostResult(fortmatResponse(result));
+    } catch (err) {
+      setPostResult(err.message);
+    }
+  }
+  const clearPostOutput = () => {
+    setPostResult(null);
+  };
+  return (
+    <div>
+      <form>
+        <input type="number" placeholder="Add film id" ref={film_film_id} />
+      </form>
+      <form>
+        <textarea
+          type="text"
+          placeholder="Add your review here..."
+          ref={post_review}
+        />
+      </form>
+      <button onClick={postData}>Save</button>
+    </div>
+  );
+}
+
+function DeleteReview() {
+  const baseURL = "http://localhost:8080/HomePage";
+  const delete_id = useRef(null);
+  const [deleteResult, setDeleteResult] = useState(null);
+  const fortmatResponse = (res) => {
+    return JSON.stringify(res, null, 2);
+  };
+
+  async function deleteReviewId() {
+    const id = delete_id.current.value;
+    if (id) {
+      try {
+        const res = await fetch(`${baseURL}/RemoveReviews/${id}`, {
+          method: "delete",
+        });
+        const data = await res.json();
+        const result = {
+          status: res.status + "-" + res.statusText,
+          headers: { "Content-Type": res.headers.get("Content-Type") },
+          data: data,
+        };
+        setDeleteResult(fortmatResponse(result));
+      } catch (err) {
+        setDeleteResult(err.message);
+      }
+    }
+  }
+  const clearDeleteOutput = () => {
+    setDeleteResult(null);
+  };
+  return (
+    <div>
+      <form>
+        <input type="number" placeholder="Review id" ref={delete_id} />
+      </form>
+      <button onClick={deleteReviewId}>Delete</button>
+    </div>
+  );
+}
+
+function UpdateReview() {
+  const baseURL = "http://localhost:8080/HomePage";
+  const review_id = useRef(null);
+  const update_review = useRef(null);
+  const [updateResult, setUpdateResult] = useState(null);
+  const fortmatResponse = (res) => {
+    return JSON.stringify(res, null, 2);
+  };
+  async function updateReviewId() {
+    const id = review_id.current.value;
+    const rev = update_review.current.value;
+    if (id) {
+      const updateData = {
+        review: update_review.current.value,
+      };
+      try {
+        const res = await fetch(
+          `${baseURL}/UpdateReviews/${id}?customer_review=${rev}`,
+          {
+            method: "put",
+            headers: {
+              "Content-Type": "application/json",
+              "x-access-token": "token-value",
+            },
+            body: JSON.stringify(updateData),
+          }
+        );
+        if (!res.ok) {
+          const message = `An error has occured: ${res.status} - ${res.statusText}`;
+          throw new Error(message);
+        }
+        const data = await res.json();
+        const result = {
+          status: res.status + "-" + res.statusText,
+          headers: { "Content-Type": res.headers.get("Content-Type") },
+          data: data,
+        };
+        setUpdateResult(fortmatResponse(result));
+      } catch (err) {
+        setUpdateResult(err.message);
+      }
+    }
+  }
+  const clearUpdateOutput = () => {
+    setUpdateResult(null);
+  };
+  return (
+    <div>
+      <form>
+        <input type="number" placeholder="Review id" ref={review_id} />
+      </form>
+      <form>
+        <textarea
+          type="text"
+          placeholder="Update your review here..."
+          ref={update_review}
+        />
+      </form>
+      <button onClick={updateReviewId}>Save</button>
+    </div>
+  );
+}
+
 class FilmRow extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      customer_review: "",
+    };
+  }
+
+  // componentDidMount() {
+
+  //   const requestOptions = {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ postReview }),
+  //   };
+  //   fetch("http://localhost:8080/HomePage/AddReviews", requestOptions)
+  //   .then((response) => response.json())
+  //   .then((data) => this.setState({ 'Success': customer_review }));
+  // }
+
   render() {
-    const film = this.props.film;
+    const filmdata = this.props.movies;
+    const customer_review = this.AddReview;
+    const postData = this.state.postData;
+    // const changeStatus = this.props.FilterableFilmTable;
+    // let showComponent = this.props.showComponent;
 
     return (
       <k>
-        <button id="select-film">
+        {/* <button className="select-film" onClick={changeStatus}> */}
+        <div className="select-film">
           <dt>
             <br />
-            <h3>{film.title}</h3>
+            <h3>{filmdata.title}</h3>
           </dt>
-          <dd>{film.release_year}</dd>
+          <dt>{filmdata.release_year}</dt>
           <br />
-        </button>
+          <dt>{filmdata.description}</dt>
+          <br />
+          {filmdata.reviews.map((filmReviews) => (
+            <div>
+              {filmReviews.review_id} - {filmReviews.customer_review}
+            </div>
+          ))}
+          <br />
+          {/* <form>
+            <textarea
+              type="text"
+              placeholder="Add your review here..."
+              ref={customer_review}
+            />
+          </form>
+          <button onClick={postData}>Save</button> */}
+          {/* <AddReview /> */}
+        </div>
+        {/* </button> */}
       </k>
     );
   }
 }
 
 class FilmTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      top: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/HomePage/AllFilms")
+      .then((response) => response.json())
+      .then((response) => this.setState({ top: response }));
+  }
+
   render() {
     const filterText = this.props.filterText.toLowerCase();
 
     const rows = [];
 
-    this.props.films.forEach((film) => {
-      if (film.title.toLowerCase().indexOf(filterText) === -1) {
+    this.state.top.forEach((movies) => {
+      if (movies.title.toLowerCase().indexOf(filterText) === -1) {
         return;
       }
-
-      rows.push(<FilmRow film={film} key={film.title} />);
+      rows.push(<FilmRow movies={movies} key={movies.film_id} />);
     });
+    return <dl id="films">{rows}</dl>;
+  }
+}
+
+class FilmInfo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+    };
+  }
+
+  render() {
+    const film = this.props.film;
+    let show = this.state.show;
+
+    if ((show = 1)) {
+      return (
+        <div id="film-info">
+          <FilmTitle />
+          <br />
+          <FilmDescription />
+          <br />
+          <ActorsTable actors={this.props.actors} />
+          <ReviewsTable reviews={this.props.reviews} />
+          <br />
+          <ReviewButton />
+        </div>
+      );
+    }
+  }
+}
+
+class FilmTitle extends React.Component {
+  render() {
+    const filmdata = this.props.movies;
     return (
-      <dl>{rows}</dl>
-      //   <table>
-      //     <tbody>{rows}</tbody>
-      //   </table>
+      <div id="film-title">
+        <h1>title</h1>
+        <p>Release Year</p>
+      </div>
     );
   }
 }
 
-// class FilmInfo extends React.Component {
-//   filmButton = "select-film";
-//   render() {
-//     return (filmButton = addEventListener("click", (evt) => {
-//       <div id="welcome">
-//         <FilmTitle />
-//         {/* <SearchBar
-//             filterText={this.state.filterText}
-//             onFilterTextChange={this.handleFilterTextChange}
-//           />
-//           <br></br>
-//           <FilmTable
-//             films={this.props.films}
-//             filterText={this.state.filterText}
-//           /> */}
-//       </div>;
-//     }));
-//   }
-// }
+class FilmDescription extends React.Component {
+  render() {
+    return (
+      <div id="description">
+        film description film description film description film description film
+        description film description film description film description film
+        description film description film description film description film
+        description film description film description film description film
+        description film description film description film description
+      </div>
+    );
+  }
+}
 
-class FilmTitle extends React.Component {
+class ActorsTable extends React.Component {
+  render() {
+    const actorRows = [];
+
+    this.props.actors.forEach((actor) => {
+      actorRows.push(<ActorsRow actor={actor} key={actor.first_name} />);
+    });
+    return <dl id="actors">{actorRows}</dl>;
+  }
+}
+
+class ActorsRow extends React.Component {
+  render() {
+    const actor = this.props.actor;
+
+    return (
+      <l>
+        <dt>
+          <h3>
+            {actor.first_name} {actor.last_name}
+          </h3>
+        </dt>
+      </l>
+    );
+  }
+}
+
+class ReviewsTable extends React.Component {
+  render() {
+    const reviewRows = [];
+
+    this.props.reviews.forEach((review) => {
+      reviewRows.push(<ReviewsRow review={review} key={review.review} />);
+    });
+    return (
+      <ol id="reviews">
+        <ld>{reviewRows}</ld>
+      </ol>
+    );
+  }
+}
+
+class ReviewsRow extends React.Component {
+  render() {
+    const review = this.props.review;
+    return (
+      <li>
+        <ld>{review.review}</ld>
+      </li>
+    );
+  }
+}
+
+class ReviewButton extends React.Component {
   render() {
     return (
       <div>
-        <h1>Film Title</h1>
-        <h3>film description</h3>
+        <button id="add-review">Leave a Review</button>
+      </div>
+    );
+  }
+}
+
+class Review extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+    };
+  }
+  render() {
+    return (
+      <div id="review">
+        <FilmTitle />
+        <br />
+        <FilmDescription />
+        <br />
+        <ReviewBox />
+        <br />
+        <SaveReview />
+      </div>
+    );
+  }
+}
+
+class ReviewBox extends React.Component {
+  render() {
+    return (
+      <form id="review-box">
+        <textarea type="text" placeholder="Add your review here..." />
+      </form>
+    );
+  }
+}
+
+class SaveReview extends React.Component {
+  render() {
+    return (
+      <div>
+        <button id="save-review">Save</button>
       </div>
     );
   }
@@ -139,27 +542,66 @@ const FILMS = [
   {
     release_year: "2006",
     title: "ACADEMY DINOSAUR",
+    film_id: "1",
   },
   {
     release_year: "2006",
     title: "ACE GOLDFINGER",
+    film_id: "2",
   },
   {
     release_year: "2006",
     title: "ADAPTATION HOLES",
+    film_id: "3",
   },
   {
     release_year: "2006",
     title: "AFFAIR PREJUDICE",
+    film_id: "4",
   },
   {
     release_year: "2006",
     title: "AFRICAN EGG",
+    film_id: "5",
   },
-  { release_year: "2006", title: "AGENT TRUMAN" },
+  { release_year: "2006", title: "AGENT TRUMAN", film_id: "6" },
+];
+
+const ACTORS = [
+  {
+    first_name: "Orestas",
+    last_name: "Dulinskas",
+  },
+  {
+    first_name: "Adam",
+    last_name: "Bryant",
+  },
+  {
+    first_name: "M",
+    last_name: "C",
+  },
+  {
+    first_name: "Chelise",
+    last_name: "Talbot",
+  },
+];
+
+const REVIEWS = [
+  {
+    review:
+      "Autism spectrum disorder (ASD) is a developmental disability caused by differences in the brain. Some people with ASD have a known difference, such as a genetic condition. Other causes are not yet known. Scientists believe there are multiple causes of ASD that act together to change the most common ways people develop. We still have much to learn about these causes and how they impact people with ASD.",
+  },
+  {
+    review:
+      "Autism spectrum disorder (ASD) is a developmental disability caused by differences in the brain. Some people with ASD have a known difference, such as a genetic condition. Other causes are not yet known. Scientists believe there are multiple causes of ASD that act together to change the most common ways people develop. We still have much to learn about these causes and how they impact people with ASD.",
+  },
+  {
+    review:
+      "Autism spectrum disorder (ASD) is a developmental disability caused by differences in the brain. Some people with ASD have a known difference, such as a genetic condition. Other causes are not yet known. Scientists believe there are multiple causes of ASD that act together to change the most common ways people develop. We still have much to learn about these causes and how they impact people with ASD.",
+  },
 ];
 
 ReactDOM.render(
-  <FilterableFilmTable films={FILMS} />,
+  <FilterableFilmTable films={FILMS} actors={ACTORS} reviews={REVIEWS} />,
   document.getElementById("root")
 );
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useState } from "react";
+import axios from "axios";
 import "./index.css";
 
 class FilterableFilmTable extends React.Component {
@@ -7,9 +9,30 @@ class FilterableFilmTable extends React.Component {
     super(props);
     this.state = {
       filterText: "",
+      name: "React",
+      showFilmTable: true,
+      showFilmInfo: false,
     };
 
+    this.showComponent = this.showComponent.bind(this);
     this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+
+    // handleShowFilmTable(film) {
+    //   this.setState({ categoryClicked: true });
+    //   this.setState({ category: filmCategory });
+
+    //   console.log(this.state.category);
+  }
+
+  showComponent(name) {
+    console.log(name);
+    switch (name) {
+      case "showFilmTable":
+        this.setState({ showFilmTable: !this.state.showFilmTable });
+        break;
+      case "showFilmInfo":
+        this.setState({ showFilmInfo: !this.state.showFilmInfo });
+    }
   }
 
   handleFilterTextChange(FT) {
@@ -18,7 +41,13 @@ class FilterableFilmTable extends React.Component {
     });
   }
 
+  changeStatus() {
+    return <FilmRow onClick={() => this.showComponent()} />;
+  }
+
   render() {
+    const { showFilmTable, showFilmInfo } = this.state;
+
     return (
       <div id="welcome">
         <Title />
@@ -27,10 +56,14 @@ class FilterableFilmTable extends React.Component {
           onFilterTextChange={this.handleFilterTextChange}
         />
         <br></br>
-        <FilmTable
-          films={this.props.films}
-          filterText={this.state.filterText}
-        />
+        {showFilmInfo && <FilmInfo />}
+        {showFilmTable && (
+          <FilmTable
+            filmRepo={this.state.filmRepo}
+            filterText={this.state.filterText}
+            onClick={() => this.showComponent()}
+          />
+        )}
       </div>
     );
   }
@@ -41,7 +74,7 @@ class Title extends React.Component {
     return (
       <div>
         <h1>Critique</h1>
-        <h3>Website description</h3>
+        <h3>website description</h3>
       </div>
     );
   }
@@ -63,70 +96,126 @@ class SearchBar extends React.Component {
   }
 }
 
+// function show() {
+//   onClick={() => this.showComponent("showFilmTable")}
+// };
+
 class FilmRow extends React.Component {
   render() {
-    const film = this.props.film;
-
+    const film = this.state.filmPackages;
+    // let showComponent = this.props.showComponent;
     return (
       <k>
-        <dt>
+        <button className="select-film" onClick={() => this.props.onClick()}>
+          <dt>
+            <br />
+            <h3>{film.title}</h3>
+          </dt>
+          <dd>{film.release_date}</dd>
           <br />
-          <h3>{film.name}</h3>
-        </dt>
-        <dd>{film.price}</dd>
-        <br />
+        </button>
       </k>
     );
   }
 }
 
 class FilmTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      filmPackages: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:8080/HomePage/AllFilms").then((res) =>
+      this.setState({
+        filmPackages: res.data,
+      })
+    );
+  }
+
   render() {
     const filterText = this.props.filterText.toLowerCase();
 
     const rows = [];
 
-    this.props.films.forEach((film) => {
-      if (film.name.toLowerCase().indexOf(filterText) === -1) {
+    this.state.filmPackages.forEach((film) => {
+      if (film.res.data.toLowerCase().indexOf(filterText) === -1) {
         return;
       }
 
-      rows.push(<FilmRow film={film} key={film.name} />);
+      rows.push(
+        <FilmRow
+          film={film}
+          key={film.title}
+          onClick={(i) => this.props.onClick(film)} //////////////////////
+        />
+      );
     });
-    return (
-      <dl>{rows}</dl>
-      //   <table>
-      //     <tbody>{rows}</tbody>
-      //   </table>
-    );
+    return <dl>{rows}</dl>;
   }
 }
 
-const FILMS = [
-  {
-    price: "2006",
-    name: "ACADEMY DINOSAUR",
-  },
-  {
-    price: "2006",
-    name: "ACE GOLDFINGER",
-  },
-  {
-    price: "2006",
-    name: "ADAPTATION HOLES",
-  },
-  {
-    price: "2006",
-    name: "AFFAIR PREJUDICE",
-  },
-  {
-    price: "2006",
-    name: "AFRICAN EGG",
-  },
-  { price: "2006", name: "AGENT TRUMAN" },
-];
+class FilmInfo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+    };
+  }
+  render() {
+    const film = this.props.film;
+    let show = this.state.show;
+
+    if ((show = 1)) {
+      return (
+        <div id="film-info">
+          <h1>test</h1>
+        </div>
+      );
+    }
+  }
+}
+
+// const FILMS = () => {
+//   fetch("http://50.17.38.181:8080/HomePage/AllFilms")
+//     .then((response) => response.json())
+//     .then((jsonData) => console.log(jsonData));
+// }
+
+// const FILMS = [
+//   {
+//     release_year: "2006",
+//     title: "ACADEMY DINOSAUR",
+//     id: 1,
+//   },
+//   {
+//     release_year: "2006",
+//     title: "ACE GOLDFINGER",
+//     id: 2,
+//   },
+//   {
+//     release_year: "2006",
+//     title: "ADAPTATION HOLES",
+//     id: 3,
+//   },
+//   {
+//     release_year: "2006",
+//     title: "AFFAIR PREJUDICE",
+//     id: 4,
+//   },
+//   {
+//     release_year: "2006",
+//     title: "AFRICAN EGG",
+//     id: 5,
+//   },
+//   { release_year: "2006", title: "AGENT TRUMAN", id: 6 },
+// ];
 
 ReactDOM.render(
-  <FilterableFilmTable films={FILMS} />,
+  <FilterableFilmTable />,
+  // <WebsiteBody />,
   document.getElementById("root")
 );
